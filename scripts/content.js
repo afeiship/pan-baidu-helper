97796da04b47399be7f4e3c4b274c05823963383
afeiship <1290657123@qq.com>
2020-03-22 15:10:26 +0800

    fix: click step by step

--- scripts/content.js
+++ scripts/content.js
@@ -13,3 +13,12 @@ $('[data-key="server_filename"] > div').click();
 setTimeout(() => {
   $('[title=保存到我的百度网盘]').click();
 }, 100);
+
+setTimeout(() => {
+  console.log('save path item - 300s');
+  $('.save-path-item').click();
+}, 300);
+
+setTimeout(() => {
+  $('.g-button-blue-large .g-button-right').click();
+}, 500);
